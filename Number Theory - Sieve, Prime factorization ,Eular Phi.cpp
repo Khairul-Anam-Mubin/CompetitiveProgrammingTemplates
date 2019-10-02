6fe802b6cedfca38bbf1cebe920d9d28f2350455
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-10-02 14:49:21 +0600

    Update Number Theory - Sieve, Prime factorization ,Eular Phi.cpp

--- Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
+++ Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
@@ -44,6 +44,29 @@ int sumofcoprimesN(int n){
     int ans = (x * n) / 2 ;
     return ans;
 }
+// returning sum of divisors of N
+int SOD( int n ) {
+    int res = 1;
+    int sqrtn = sqrt ( n );
+    for ( int i = 0; i < prime.size() && prime[i] <= sqrtn; i++ ) {
+        if ( n % prime[i] == 0 ) {
+            int tempSum = 1; // Contains value of (p^0+p^1+...p^a)
+            int p = 1;
+            while ( n % prime[i] == 0 ) {
+                n /= prime[i];
+                p *= prime[i];
+                tempSum += p;
+            }
+            sqrtn = sqrt ( n );
+            res *= tempSum;
+        }
+    }
+    if ( n != 1 ) {
+        res *= ( n + 1 ); // Need to multiply (p^0+p^1)
+    }
+    return res;
+}
+
 void FactorialFactorisation(ll n) {
     dp[1] = 0 ;
     for(int i = 2 ; i <= mx - 9 ; i++) {
