1af9840b0b91ac8a35b7a049856a3ce157c6790e
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-10-02 17:26:26 +0600

    Update Number Theory - Sieve, Prime factorization ,Eular Phi.cpp

--- Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
+++ Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
@@ -105,6 +105,41 @@ int sumofcoprimesN(int n){
     return ans;
 }
 
+// Linear Diophantine Equation
+// Given the value of integers A,B and C find a pair of integers (x,y) such that it satisfies the equation Ax+By=C.
+bool linearDiophantine ( int A, int B, int C, int *x, int *y ) {
+    int g = gcd ( A, B );
+    if ( C % g != 0 ) return false;  //No Solution
+ 
+    int a = A / g, b = B / g, c = C / g;
+    ext_gcd( a, b, x, y );  //Solve ax + by = 1
+ 
+    if ( g < 0 ) {  //Make Sure gcd(a,b) = 1
+        a *= -1; b *= -1; c *= -1;
+    }
+ 
+    *x *= c; *y *= c;  //ax + by = c
+    return true;  //Solution Exists
+} 
+int main () {
+    int x, y, A = 2, B = 3, C = 5;
+    bool res = linearDiophantine ( A, B, C, &x, &y );
+ 
+    if ( res == false ) printf ( "No Solution\n" );
+    else {
+        printf ( "One Possible Solution (%d %d)\n", x, y );
+ 
+        int g = gcd ( A, B );
+ 
+        int k = 1; //Use different value of k to get different solutions
+        printf ( "Another Possible Solution (%d %d)\n", x + k * ( B / g ), y - k * ( A / g ) );
+    }
+ 
+ return 0;
+}
+****//
+    
+    
 void FactorialFactorisation(ll n) {
     dp[1] = 0 ;
     for(int i = 2 ; i <= mx - 9 ; i++) {
