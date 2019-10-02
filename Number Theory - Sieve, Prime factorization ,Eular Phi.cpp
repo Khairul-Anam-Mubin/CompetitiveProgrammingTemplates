ee5a67d9ac3491812f59ac45210905450de207d1
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-10-02 15:14:51 +0600

    Update Number Theory - Sieve, Prime factorization ,Eular Phi.cpp

--- Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
+++ Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
@@ -67,6 +67,28 @@ int SOD( int n ) {
     return res;
 }
 
+// returning number of divisors of N
+int NOD ( int n ) {
+    int sqrtn = sqrt ( n );
+    int res = 1;
+    for ( int i = 0; i < prime.size() && prime[i] <= sqrtn; i++ ) {
+        if ( n % prime[i] == 0 ) {
+            int p = 0; // Counter for power of prime
+            while ( n % prime[i] == 0 ) {
+                n /= prime[i];
+                p++;
+            }
+            sqrtn = sqrt ( n );
+            p++; // Increase it by one at end
+            res *= p; // Multiply with answer
+        }
+    }
+    if ( n != 1 ) {
+        res *= 2; // Remaining prime has power p^1. So multiply with 2/
+    }
+    return res;
+}
+
 void FactorialFactorisation(ll n) {
     dp[1] = 0 ;
     for(int i = 2 ; i <= mx - 9 ; i++) {
