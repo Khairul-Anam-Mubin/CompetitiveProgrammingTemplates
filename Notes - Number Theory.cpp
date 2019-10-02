ccbde5a72601328f3dc58158125d085a48db8411
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-10-02 14:49:28 +0600

    Update Notes - Number Theory.cpp

--- Notes - Number Theory.cpp
+++ Notes - Number Theory.cpp
@@ -1,2 +1,24 @@
-1 : For non-negative integers a and b, where a and b are not both zero, provable by considering the Euclidean algorithm in base n :
-gcd((n^a) − 1, (n^b) − 1) = (n^gcd(a,b)) − 1
+1 : GCD property :  
+    For non-negative integers a and b, where a and b are not both zero, provable by considering the Euclidean algorithm in base n :
+
+    // gcd((n^a) − 1, (n^b) − 1) = (n^gcd(a,b)) − 1
+
+2 : GCD and LCM relations , It is based on the formula that , 
+  
+    // LCM(A , B) × GCD( A , B) = A × B
+    
+3 : Number of divisors (NOD) of a number N can be calculated using Prime power factorization.
+    Let , N = P1^a1 * P2^a2 * P3^a3 * ... * Pn^ak , is the prime power factorization of a number N , where P is the prime number and
+    a is number of times occurs that prime number.
+    Then , NOD(N) defines as :
+
+    // NOD(N) = (a1 + 1) * (a2 + 1) * (a3 + 1) * ... * (ak + 1)
+
+4 : Sum of divisors (SOD) of a number N can be calculated using prime power factorization.
+   Let ,  N = P1^a1 * P2^a2 * P3^a3 * ... * Pn^ak , s the prime power factorization of a number N , where P is the prime number and
+    a is number of times occurs that prime number.
+  Then , SOD(N) defines as :
+  
+    // SOD(N) = (P1^0 + P1^1 + p1^2 + ... + P1^a1) * (P2^0 + P2^1 + P2^2 + ... + P2^a2) * (Pk^0 + Pk^1 + ... + Pk^ak)
+
+5 : 
