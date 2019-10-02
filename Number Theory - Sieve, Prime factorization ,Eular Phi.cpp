9df04bf3f81184af3145ba9b6368a01ab53b38af
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-10-02 18:15:30 +0600

    Update Number Theory - Sieve, Prime factorization ,Eular Phi.cpp

--- Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
+++ Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
@@ -139,7 +139,7 @@ int main () {
 }
 ****//
 
-// leading zero..
+// leading numbers ..
 int main() {
     int tc ;
     scanf("%d",&tc) ;
@@ -195,4 +195,27 @@ void factFactorize ( int n ) {
         }
         printf ( "%d^%d\n", prime[i], freq );
     }
+}
+
+// Find the first K digits of N!
+int leadingDigitFact ( int n, int k ) {
+    double fact = 0;
+ 
+    // Find log(N!)
+    for ( int i = 1; i <= n; i++ ) {
+        fact += log10 ( i );
+    }
+ 
+    // Find the value of q
+    double q = fact - floor ( fact+eps );
+ 
+    double B = pow ( 10, q );
+ 
+    // Shift decimal point k-1 \times
+    for ( int i = 0; i < k - 1; i++ ) {
+        B *= 10;
+    }
+ 
+    // Don't forget to floor it
+    return floor(B+eps);
 }
