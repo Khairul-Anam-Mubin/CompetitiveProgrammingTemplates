445f94e0968670d113190bb433c887eb8c03db1a
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-10-03 14:14:22 +0600

    Update Number Theory - Sieve, Prime factorization ,Eular Phi.cpp

--- Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
+++ Number Theory - Sieve, Prime factorization ,Eular Phi.cpp
@@ -219,3 +219,49 @@ int leadingDigitFact ( int n, int k ) {
     // Don't forget to floor it
     return floor(B+eps);
 }
+
+// Chinese remainder theorem...
+// Given two sequences of numbers A=[a1,a2,…,an] and M=[m1,m2,…,mn], 
+//find the smallest solution to the following linear congruence equations if it exists:
+    x ≡ a1(mod m1)
+    x ≡ a2(mod m2)
+    …
+    x ≡ an(mod mn)
+        
+/** Works for non-coprime moduli.
+ Returns {-1,-1} if solution does not exist or input is invalid.
+ Otherwise, returns {x,L}, where x is the solution unique to mod L
+*/
+pair<int, int> chinese_remainder_theorem( vector<int> A, vector<int> M ) {
+    if(A.size() != M.size()) return {-1,-1}; /** Invalid input*/
+ 
+    int n = A.size();
+ 
+    int a1 = A[0];
+    int m1 = M[0];
+    /** Initially x = a_0 (mod m_0)*/
+ 
+    /** Merge the solution with remaining equations */
+    for ( int i = 1; i < n; i++ ) {
+        int a2 = A[i];
+        int m2 = M[i];
+ 
+        int g = __gcd(m1, m2);
+        if ( a1 % g != a2 % g ) return {-1,-1}; /** No solution exists*/
+ 
+        /** Merge the two equations*/
+        int p, q;
+        ext_gcd(m1/g, m2/g, &p, &q);
+ 
+        int mod = m1 / g * m2; /** LCM of m1 and m2*/
+ 
+        /** We need to be careful about overflow, but I did not bother about overflow here to keep the code simple.*/
+        int x = (a1*(m2/g)*q + a2*(m1/g)*p) % mod;
+ 
+        /** Merged equation*/
+        a1 = x;
+        if (a1 < 0) a1 += mod; /** Result is not suppose to be negative*/
+        m1 = mod;
+    }
+    return {a1, m1};
+}
