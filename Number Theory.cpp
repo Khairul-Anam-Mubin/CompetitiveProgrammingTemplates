8297d499b78f809fc50c93ed8339c0a3de2bda98
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-10-16 17:11:03 +0600

    Update Number Theory.cpp

--- Number Theory.cpp
+++ Number Theory.cpp
@@ -21,6 +21,23 @@ void sieve(){
             prime.pb(i);
 }
 
+// SubLinearSieve
+// o(n) complexity
+const int N = 10000000;
+int lp[N+1];
+vector<int> pr;
+
+void LinearSieve() {
+    for (int i=2; i<=N; ++i) {
+        if (lp[i] == 0) {
+            lp[i] = i;
+            pr.push_back (i);
+        }
+        for (int j = 0 ; j < (int)pr.size() && pr[j] <= lp[i] && i * pr[j] <= N ; ++j)
+            lp[i * pr[j]] = pr[j];
+    }
+}
+
 // returning sum of divisors of N
 // complexity of optimized prime factorization..
 int SOD( int n ) {
