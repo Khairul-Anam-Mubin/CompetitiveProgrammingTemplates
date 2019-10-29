5b3f8019e5d183928c7b18a39c6aad3cb7a14efe
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-10-29 23:53:33 +0600

    Update Strings - KMP.cpp

--- Strings - KMP.cpp
+++ Strings - KMP.cpp
@@ -56,3 +56,66 @@ int main() {
     cout << Kmp(text , pattern) << "\n" ; ;
     return 0 ;
 }
+
+// Freq counting....
+// 1255 - Substring Frequency
+// Light OJ
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+char pattern[1000001] , text[1000001] ;
+int pfix[1000001] ;
+void BuildPrefixArray() {
+    int len = strlen(pattern) ;
+    pfix[0] = 0 ;
+    for(int i = 1 , j = 0 ; i < len ; ) {
+        if(pattern[i] == pattern[j]) {
+            pfix[i] = j + 1 ;
+            i++ ;
+            j++ ;
+        }
+        else {
+            if(j == 0) {
+                pfix[i] = 0 ;
+                i++ ;
+            }
+            else {
+                j = pfix[j - 1] ;
+            }
+        }
+    } 
+}
+int KMP() {
+    BuildPrefixArray() ;
+    int len1 = strlen(text) ;
+    int len2 = strlen(pattern) ;
+    int cnt = 0 ;
+    for(int i = 0 , j = 0 ; i < len1 && j < len2 ; ) {
+        if(text[i] == pattern[j]) {
+            i++ ;
+            j++ ;
+        }
+        else {
+            if(j == 0) 
+                i++ ;
+            else
+                j = pfix[j - 1] ;
+        }
+        if(j == len2) {
+            cnt++ ;
+            j = pfix[j - 1] ;
+        }
+    }
+    return cnt ;
+}
+int main() {
+    int tc , test = 0 ;
+    scanf("%d" ,&tc) ;
+    while(tc--) {
+        scanf("%s%s" ,text , pattern) ;
+        int ans = KMP() ;
+        printf("Case %d: %d\n" ,++test , ans) ; 
+    }
+    return 0 ;
+}
