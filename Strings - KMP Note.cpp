cba21e3bb53aa7d4365fa36f035540f381aad477
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-11-07 17:50:28 +0600

    Update and rename Strings - Note.cpp to Strings - KMP Note.cpp

---
+++ Strings - KMP Note.cpp
@@ -0,0 +1,6 @@
+1. Search for a substring in a string. The Knuth-Morris-Pratt algorithm
+2 . Counting the number of occurrences of each prefix
+3 . Let us compute the prefix function for s. 
+    Using the last value of it we define the value k=n−π[n−1]. 
+    We will show, that if k divides n, then k will be the answer, 
+    otherwise there doesn't exists an effective compression and the answer is n .
