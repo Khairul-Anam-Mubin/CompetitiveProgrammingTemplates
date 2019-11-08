57559331efa22b41bdb1b8d0e14a7256f8d46145
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-11-08 16:51:01 +0600

    Update Number Theory.cpp

--- Number Theory.cpp
+++ Number Theory.cpp
@@ -254,6 +254,19 @@ void factFactorize ( int n ) {
         printf ( "%d^%d\n", prime[i], freq );
     }
 }
+// n! mod p 
+// complexity PlogpN
+int factmod(int n, int p) {
+    int res = 1;
+    while (n > 1) {
+        res = (res * ((n/p) % 2 ?  p-1 : 1)) % p;
+        for (int i = 2; i <= n%p; ++i)
+            res = (res * i) % p;
+        n /= p;
+    }
+    return res % p;
+}
+
 
 // Find the first K digits of N!
 int leadingDigitFact ( int n, int k ) {
