24ec211bd9bb3815ebc1533edc1e1cca13eb1185
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-11-08 12:58:12 +0600

    Update Number Theory.cpp

--- Number Theory.cpp
+++ Number Theory.cpp
@@ -371,3 +371,17 @@ vector<bool> segmented(ll a , ll b)
     }
     return isp2;
 }
+
+// The above code returns Fn and Fn+1 as a pair..
+pair<int, int> fib (int n) {
+    if (n == 0)
+        return {0, 1};
+
+    auto p = fib(n >> 1);
+    int c = p.first * (2 * p.second - p.first);
+    int d = p.first * p.first + p.second * p.second;
+    if (n & 1)
+        return {d, c + d};
+    else
+        return {c, d};
+}
