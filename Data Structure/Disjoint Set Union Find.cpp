65ced8347a561fe7daa6146a9584a93895fd9940
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-12-02 02:06:26 +0600

    Create Disjoint Set Union Find.cpp

---
+++ Data Structure/Disjoint Set Union Find.cpp
@@ -0,0 +1,52 @@
+/*************************Disjoint Set Union Find**************************/
+
+/*.................Number of connecting components..............*/
+/* Given N people and M groups , how many people are in same components or How many
+people are connect to a certain node.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int dsu[1000000]; // disjoint set array
+int tot[1000000]; // Size array
+//Make set operation for initialize. Firstly there are N sets indicating and all are size (tot) = 1.
+void makeset(int n){
+    for(int i = 0 ; i <= n ; i++){
+        dsu[i] = i;
+        tot[i] = 1;
+    }
+}
+//This Function finds parent and mark all child as same parent and also doing path compression 
+int Find(int child){
+    if(dsu[child] == child)
+        return child;
+    return dsu[child] = Find(dsu[child]);
+}
+//Simply Unions two set and memorizing its size in its root index of tot..
+void Union(int x,int y){
+    x = Find(x);
+    y = Find(y);
+    if(x != y){
+        dsu[x] = y;
+        tot[y] += tot[x];
+    }
+}
+int main() {
+    int n , m ;
+    cin >> n >> m;
+    makeset(n);
+    while(m--) {
+        int k ; cin >> k ;
+        if(!k) continue;
+        int first ; cin >> first;
+        for(int i = 1 ; i < k ; i++){
+            int add; cin >> add;
+            Union(first,add);
+        }
+    }
+    //Finding Total number of elements in a particular set
+    for(int i = 1 ; i <= n  ; i++)
+        cout << tot[Find(i)] << " ";
+    cout << "\n";
+    return 0;
+}
