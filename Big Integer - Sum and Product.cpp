0f42fe57078f33d8a59a374ea15437ebc62f0f02
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-12-01 22:48:59 +0600

    Update Big Integer - Sum and Product.cpp

--- Big Integer - Sum and Product.cpp
+++ Big Integer - Sum and Product.cpp
@@ -1,15 +1,12 @@
 //take to string as numbers and returns the product of these two numbers to strings
 // On worst case BigintProd take O(n^2) complexity
-string BigintProd(string str1,string str2)
-{
+string BigintProd(string str1,string str2) {
     if(str1.size() < str2.size())
         swap(str1,str2);
-    
     int len1=(int)str1.size(); 
     int len2=(int)str2.size();
     int carry=0,cnt=1;
     string prod,pro;
-
     for(int i=len2-1; i>=0; i--){
         carry=0;
         for(int j=len1-1; j>=0; j--){
@@ -30,13 +27,11 @@ string BigintProd(string str1,string str2)
             pro=BigintSum(pro,prod);
             prod.clear();
         }
-        for(int k=len2-1;k>=i;k--){
+        for(int k=len2-1;k>=i;k--)
             prod.push_back('0');
-        }
     }
     //return "0" if any multiply apply with 0
-    if(pro[0]=='0' && pro.size()>1)
-        pro="0";
+    if(pro[0]=='0' && pro.size()>1) pro="0";
     return pro;
 }
 // works in O(n) ..
@@ -44,22 +39,19 @@ string BigintSum(string str1,string str2)
 {
     if(str1.size() < str2.size())
         swap(str1,str2);
-    
     int len1=(int)str1.size(); 
     int len2=(int)str2.size();
     int n=len2;
     int carry=0;
     string sum;
-
     for(int i=len1-1, j=len2-1; i>=0 ; i--, j--){
         int x;
         if(n){
             x = str1[i] - '0' + str2[j] - '0' + carry;
             n--;
         }
-        else{
+        else
             x = str1[i] - '0' + carry;
-        }
         carry=x/10;
         x=x%10;
         sum.push_back(x+'0');
@@ -67,6 +59,5 @@ string BigintSum(string str1,string str2)
     if(carry)
         sum.push_back(carry+'0');
     reverse(sum.begin(),sum.end());
-
     return sum;
 }
