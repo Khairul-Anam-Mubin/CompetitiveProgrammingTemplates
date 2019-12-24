0b9aa41fa90718851514996ca9830abc9f529129
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-12-24 20:10:13 +0600

    Create Cycle Find.cpp

---
+++ Graph Theory/Cycle Find.cpp
@@ -0,0 +1,56 @@
+/*********************************Cycle Find**************************************/
+
+/*...........................Cycle Find in Directed Graph............................*/
+/*  given a directed graph consisting of n vertices and m edges ,is it possible to make this graph 
+acyclic by removing at most one edge from it ? */
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+vector <int> g[510] ;
+int color[510] , cycle = 0 ;
+void makeset(int n) {
+    for(int i = 0 ; i <= n ; i++)
+        color[i] = 0 ;
+}
+void dfs(int u) {
+    color[u] = 2 ;
+    for(int v : g[u]) {
+        if(color[v] == 2) { 
+            cycle++ ; // counting cycles...
+        } else if(color[v] == 0) {
+            dfs(v) ;
+        }
+    }
+    color[u] = 1 ;
+}
+int main() {
+    int n , m ;
+    cin >> n >> m ;
+    for(int i = 0 ; i < m ; i++) {
+        int u , v ;
+        cin >> u >> v ;
+        g[u].push_back(v) ;
+    }
+    makeset(n) ;
+    bool flag = 0 ;
+    for(int i = 1 ; i <= n ; i++) {
+        cycle = 0 ;
+        makeset(n) ;
+        dfs(i) ;
+        for(int j = 1 ; j <= n && cycle <= 1 ; j++) {
+            if(!color[j])
+                dfs(j) ;
+        }
+        if(cycle <= 1) {
+            flag = 1 ;
+            break ;
+        }
+    }
+    if(flag) {
+        cout << "YES\n" ;
+    } else {
+        cout << "NO\n" ;
+    }
+    return 0 ;
+}
