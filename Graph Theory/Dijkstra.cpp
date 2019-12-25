667c487a973640ba5c0d1b1133c91942ec80f51c
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-12-26 00:47:52 +0600

    Update Dijkstra.cpp

--- Graph Theory/Dijkstra.cpp
+++ Graph Theory/Dijkstra.cpp
@@ -71,3 +71,68 @@ int main() {
         cout << "-1\n" ;
     return 0 ;
 }
+
+/*..............Dijkstra - 2nd shortest path...........*/
+
+// Light oj 1099 - Not the Best
+#include <bits/stdc++.h>
+using namespace std ;
+
+#define mxN 5000
+#define INF 1e18
+
+vector <pair<int , int > > G[mxN + 10] ;
+long long dis1[mxN + 10] , dis2[mxN + 10] ;
+
+void graph_clear(int nodes) {
+    for(int i = 0 ; i <= nodes ; i++) {
+        G[i].clear() ;
+    }
+}
+void dijkstra(int start , int destination , int nodes) {
+    for(int i = 0 ; i <= nodes ; i++)
+        dis1[i] = dis2[i] = INF ;
+
+    priority_queue <pair <int , int> > pq ;
+    dis1[1] = 0 ;
+    pq.push(make_pair(0 , 1)) ;
+    
+    while(!pq.empty()) {
+        int u = pq.top().second ;
+        int wu = -pq.top().first ;
+        pq.pop() ;
+        for(int i = 0 ; i < G[u].size() ; i++) {
+            int v = G[u][i].second ;
+            int wv = G[u][i].first ;
+            if(wv + wu < dis1[v]) {
+                dis2[v] = dis1[v] ; // if current weight is less then previous then the previous could be our 2nd shortest dis
+                dis1[v] = wv + wu ;
+                pq.push(make_pair(-dis1[v] , v)) ;
+                pq.push(make_pair(-(dis1[v] + wv) , u)) ; // as considering back edge..
+            }
+            else if(wv + wu != dis1[v] && wu + wv < dis2[v]) {
+                dis2[v] = wu + wv ;
+                pq.push(make_pair(-dis2[v], v)) ;
+            }
+        }
+    }
+}
+int main() {
+    int tc ,t = 0 ;
+    scanf("%d",&tc );
+    while(tc--) {
+        int nodes , edges ;
+        scanf("%d%d",&nodes ,&edges );
+        for(int i = 0 ; i < edges ; i++) {
+            int u , v , w ;
+            scanf("%d%d%d",&u , &v, &w) ;
+            G[u].push_back(make_pair(w , v)) ;
+            G[v].push_back(make_pair(w , u)) ;
+        }
+        int start = 1 , destination = nodes ;
+        dijkstra(start , destination , nodes) ;
+        printf("Case %d: %d\n",++t , dis2[destination] );
+        graph_clear(nodes) ;
+    }
+    return 0 ;
+}
