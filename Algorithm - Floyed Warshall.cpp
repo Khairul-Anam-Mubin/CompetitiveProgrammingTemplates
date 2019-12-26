a543ac1f79de4f8ce12db98391a91310d630e8b5
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-12-26 13:19:07 +0600

    Update Algorithm - Floyed Warshall.cpp

--- Algorithm - Floyed Warshall.cpp
+++ Algorithm - Floyed Warshall.cpp
@@ -1,51 +1,40 @@
-// Floyed Warshall Algorithm
-// All pair shortest path
-// The ALgorithm..
-for(int k = 1 ; k <= 100 ; k++) {
-            for(int i = 1 ; i <= 100 ; i++) {
-                for(int j = 1 ; j <= 100 ; j++) {
-                    dp[i][j] = min(dp[i][j] , dp[i][k] + dp[k][j]) ;
-                }
-            }
-        }
+/************************All Pair Shortest Path*********************/
+/*........................ Floyd Warshal...................*/
+/* all pair shortest path and path printing.......*/
+// Complexity : O(V^3)
 
-// Uva : 821 - Page Hopping
-// Floyed Warshall
-// DP
-// all pair shortest path
+#include <bits/stdc++.h>
+using namespace std ;
 
-int main() {
-    int u , v , tc = 0 ;
-    while(scanf("%d%d",&u,&v) == 2) {
-        if(u == 0 && v == 0)
-            break ;
-        int dp[110][110] ;
-        for(int i = 0 ; i < 110 ; i++)
-            for(int j = 0 ; j < 110 ; j++)
-                dp[i][j] = MOD ;
-        dp[u][v] = 1 ;
-        while(scanf("%d%d",&u,&v) == 2) {
-            if(u == 0 && v == 0)
-                break ;
-            dp[u][v] = 1 ;
-        }
-        for(int k = 1 ; k <= 100 ; k++) {
-            for(int i = 1 ; i <= 100 ; i++) {
-                for(int j = 1 ; j <= 100 ; j++) {
-                    dp[i][j] = min(dp[i][j] , dp[i][k] + dp[k][j]) ;
-                }
-            }
-        }
-        double sum = 0 , cnt = 0 ;
-        for(int i = 1 ; i <= 100 ; i++) {
-            for(int j = 1 ; j <= 100 ; j++) {
-                if(i != j && dp[i][j] != MOD) {
-                    sum += dp[i][j] ;
-                    cnt++ ;
-                }
-            }
-        }
-        printf("Case %d: average length between pages = %0.3lf clicks\n",++tc ,sum / cnt) ;
-    } 
-    return 0 ;
+#define INF 1e18
+#define mxN 1000
+
+long long dp[mxN + 10][mxN + 10] ;
+int path[mxN + 10][mxN + 10] ;
+
+void printPath(int i, int j) {
+    if(i != j) 
+        printPath(i , path[i][j]);
+    printf(" %d", j);
 }
+void FloyedWarshall(int nodes) {
+    for(int i = 0 ; i <= nodes ; i++) {
+        for(int j = 0 ; j <= nodes ; j++) {
+            dp[i][j] = INF ;
+            path[i][j] = i ;
+            if(i == j)
+                dp[i][j] = 0 ;
+        }
+    }
+    for(int k = 1 ; k <= nodes ; k++) {
+        for(int i = 1 ; i <= nodes ; i++) {
+            for(int j = 1 ; j <= nodes ; j++) {
+                if (dp[i][k] < INF && dp[k][j] < INF) {
+                    dp[i][j] = min(dp[i][j] , dp[i][k] + dp[k][j]) ;
+                    path[i][j] = path[k][j] ;
+                }
+            }
+        }
+    }
+}
+
