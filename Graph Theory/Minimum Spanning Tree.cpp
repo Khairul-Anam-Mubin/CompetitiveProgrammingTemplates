d4cfbc573700fdbd2a870b1491e729c6f815e34b
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2019-12-26 12:52:51 +0600

    Update Minimum Spanning Tree.cpp

--- Graph Theory/Minimum Spanning Tree.cpp
+++ Graph Theory/Minimum Spanning Tree.cpp
@@ -1,4 +1,10 @@
 /**********************Minimum Spanning Tree************************/
+
+/*...................................... Note..............................*/ 
+/* For the 2nd minimum spanning tree we first have to find 1st  MST and a vis array to mark the edges which are used in making  
+the first MST , then we simply ignore each time 1 edge and try to make spanning tree and rest the minimum of them is the answer..
+MST is also used as Minimax , maximin , forest , sub graph */
+
 /*......................Prim's Algorithm........................*/
 /* Given a graph with n nodes and m edges , Find the minimum spanning tree .*/
 // Problem Uva : 11631 - Dark roads
@@ -67,6 +73,71 @@ int main() {
         }
         cout << tot - mincost << "\n" ;
         clear(nodes) ;
+    }
+    return 0 ;
+}
+/*....................Kruskal Algrithm.................*/
+/* Given a graph with n nodes and m edges , find the minimum spanning tree.*/
+// Uva : 11631 - Dark roads
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+vector <pair <int , pair <int, int> > > g ;
+
+#define mx 200010
+int dsu[mx] ;
+
+void makeset(int nodes) {
+    for(int i = 0 ; i <= nodes + 1 ; i++) 
+        dsu[i] = i ;
+}
+int Find(int x) {
+    if(x == dsu[x]) 
+        return x ;
+    return dsu[x] = Find(dsu[x]) ;
+}
+void Union(int x ,int y) {
+    x = Find(x) ;
+    y = Find(y) ;
+    if( x != y) 
+        dsu[x] = y ;
+}
+bool sameset(int u, int v) {
+    return (Find(u) == Find(v)) ; 
+}
+int KruskalAlgo(int nodes) {
+    makeset(nodes) ;
+    int cost = 0 ;
+    int cnt = 0 ;
+    sort(g.begin() , g.end()) ;
+    for(auto it : g) {
+        int u = it.second.first ;
+        int v = it.second.second ;
+        int w = it.first ;
+        if(!sameset(u , v)) {
+            cost += w ;
+            Union(u  ,v) ;
+            cnt++ ;
+        }
+        if(cnt == nodes - 1)
+            break;
+    }
+    return cost ;
+}
+int main() {
+    int nodes , edges ;
+    while(cin >> nodes >> edges && nodes && edges) {
+        int tot = 0 ; 
+        for(int i = 0 ; i < edges ; i++) {
+            int u , v , w ;
+            cin >> u >> v >> w ;
+            g.push_back(make_pair(w ,make_pair(u , v))) ;
+            tot += w ;
+        }
+        int ans = tot - KruskalAlgo(nodes) ; 
+        cout << ans << "\n" ;
+        g.clear() ;
     }
     return 0 ;
 }
