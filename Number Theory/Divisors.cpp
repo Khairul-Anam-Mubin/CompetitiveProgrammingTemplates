350f5a6f7f473289ad92308d1bef48f261eddecf
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2020-01-01 14:55:51 +0600

    Update Divisors.cpp

--- Number Theory/Divisors.cpp
+++ Number Theory/Divisors.cpp
@@ -1,4 +1,5 @@
 /*************************************Divisors********************************/
+/*...........................Find all the divisors of a number..................*/
 // Complexity : O(sqrt(n))
 
 #include <bits/stdc++.h>
@@ -14,4 +15,61 @@ void divisors(long long n) {
                 vec.push_back(n / i) ;
         }
     } 
+}
+
+/*...................NOD..................*/
+/* Finding the number of divisors of a number n */
+
+// complexity : O(nloglogn + klogn)
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+#define mxN 1000000000000
+#define mx 1000000
+bool isp[mx + 10] ;
+vector <long long> prime ;
+void sieve(){
+    isp[1] = 1 ;
+    for(int i = 4 ; i <= mx ; i += 2)
+        isp[i] = true;
+    for(int i = 3 ; i * i <= mx ; i += 2)
+        if(!isp[i])
+            for(int j = i * i ; j <= mx ; j +=(i + i))
+                isp[j] = true;
+    prime.push_back(2);
+    for(int i = 3 ; i <= mx ; i += 2)
+        if(!isp[i])
+            prime.push_back(i);
+}
+long long NOD ( long long n ) {
+    long long res = 1 ;
+    for ( int i = 0 ; i < prime.size() && prime[i] * prime[i] <= n ; i++ ) {
+        if ( n % prime[i] == 0LL ) {
+            long long p = 0 ; // Counter for power of prime
+            while ( n % prime[i] == 0LL ) {
+                n /= prime[i];
+                p++;
+            }
+            p++; // Increase it by one at end
+            res *= p; // Multiply with answer
+        }
+    }
+    if ( n != 1 ) {
+        res *= 2LL ; // Remaining prime has power p^1. So multiply with 2/
+    }
+    return res ;
+}
+int main() {
+    sieve() ;
+    int tc , test = 0 ;
+    cin >> tc ;
+    while(tc--) {
+        long long n ;
+        cin >> n ;
+        cout << "Case " << ++test << ": " ;
+        long long ans = NOD(n) - 1LL ;
+        cout << ans << "\n" ;
+    }
+    return 0 ;
 }
