0280277d26093d0016307140907f48fdd057df5c
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2020-01-01 13:06:04 +0600

    Create Eulers Phi.cpp

---
+++ Number Theory/Eulers Phi.cpp
@@ -0,0 +1,46 @@
+/**********************************Eulers Phi********************************/
+/*.............................Genereationg co-primes..........................*/
+
+/* Given a range of a , b <= 5*10^6 and have to find sum of all phi(i) * phi(i) in range a to b.*/
+
+#include <bits/stdc++.h>
+using namespace std ;
+
+#define mxN 5000000
+unsigned long long phi[mxN + 10] ;
+
+void PreCalPhi() {
+    // generating....
+    phi[0] = 0 ;
+    phi[1] = 1 ;
+    for(int i = 2 ; i <= mxN ; i++) {
+        if(phi[i] == 0) {
+            phi[i] = i - 1 ;             
+            for(int j = i + i ; j <= mxN ; j += i) {
+                if(phi[j] == 0)
+                    phi[j] = j ;
+                phi[j] -= (phi[j] / i) ;
+            }
+        }
+    }
+    
+    for(int i = 1 ; i <= mxN ; i++)
+        phi[i] = (phi[i] * phi[i]) ;
+    // Cumulative sum for range sum query...
+    for(int i = 1 ; i <= mxN ; i++)
+        phi[i] += phi[i - 1] ;
+}
+int main() {
+    PreCalPhi() ;
+    int tc , test = 0 ;
+    cin >> tc ;
+    while(tc--) {
+        int a , b ;
+        cin >> a >> b ;
+        if(a > b)
+            swap(a , b) ;
+        cout << "Case " << ++test << ": " ;
+        cout << phi[b] - phi[a - 1] << "\n" ;
+    }
+    return 0 ;
+}
