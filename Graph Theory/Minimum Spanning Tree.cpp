fb0c91e7511df2b756e88ac1414d3af279372075
Khairul Anam Mubin <39878277+Khairul-Anam-Mubin@users.noreply.github.com>
2020-04-02 21:17:54 +0600

    Update Minimum Spanning Tree.cpp

--- Graph Theory/Minimum Spanning Tree.cpp
+++ Graph Theory/Minimum Spanning Tree.cpp
@@ -77,67 +77,98 @@ int main() {
     return 0 ;
 }
 /*....................Kruskal Algrithm.................*/
-/* Given a graph with n nodes and m edges , find the minimum spanning tree.*/
-// Uva : 11631 - Dark roads
 
-#include <bits/stdc++.h>
-using namespace std ;
-
-vector <pair <int , pair <int, int> > > g ;
-
-#define mx 200010
-int dsu[mx] ;
-
-void makeset(int nodes) {
-    for(int i = 0 ; i <= nodes + 1 ; i++) 
-        dsu[i] = i ;
-}
-int Find(int x) {
-    if(x == dsu[x]) 
-        return x ;
-    return dsu[x] = Find(dsu[x]) ;
-}
-void Union(int x ,int y) {
-    x = Find(x) ;
-    y = Find(y) ;
-    if( x != y) 
-        dsu[x] = y ;
-}
-bool sameset(int u, int v) {
-    return (Find(u) == Find(v)) ; 
-}
-int KruskalAlgo(int nodes) {
-    makeset(nodes) ;
-    int cost = 0 ;
-    int cnt = 0 ;
-    sort(g.begin() , g.end()) ;
-    for(auto it : g) {
-        int u = it.second.first ;
-        int v = it.second.second ;
-        int w = it.first ;
-        if(!sameset(u , v)) {
-            cost += w ;
-            Union(u  ,v) ;
-            cnt++ ;
-        }
-        if(cnt == nodes - 1)
-            break;
+// mxN have to define before...
+const int mxN = 30;
+struct DSU {
+    vector <int> parent;
+    vector <int> siz;
+    DSU(int mxN) {
+        mxN <<= 1;
+        parent.resize(mxN + 1);
+        siz.resize(mxN + 1);
     }
-    return cost ;
-}
-int main() {
-    int nodes , edges ;
-    while(cin >> nodes >> edges && nodes && edges) {
-        int tot = 0 ; 
-        for(int i = 0 ; i < edges ; i++) {
-            int u , v , w ;
-            cin >> u >> v >> w ;
-            g.push_back(make_pair(w ,make_pair(u , v))) ;
-            tot += w ;
+    void Makeset(int n) {
+        for (int i = 0 ; i <= n ; i++) {
+            parent[i] = n + i;
+            parent[n + i] = n + i;
+            siz[n + i] = 1;
         }
-        int ans = tot - KruskalAlgo(nodes) ; 
-        cout << ans << "\n" ;
-        g.clear() ;
     }
-    return 0 ;
-}
+    int Find(int u) {
+        if (parent[u] == u) return u ;
+        return parent[u] = Find(parent[u]) ;
+    }
+    void Union(int u , int v) {
+        u = Find(u);
+        v = Find(v);
+        if (u != v) {
+            if (siz[u] < siz[v]) swap(u , v);
+            parent[v] = u;
+            siz[u] += siz[v];
+        }
+    }
+    bool SameSet(int u , int v) {
+        return (Find(u) == Find(v)) ;
+    }
+    void MoveUtoSetV(int u , int v) {
+        if (SameSet(u , v)) return;
+        int x = Find(u);
+        int y = Find(v);
+        siz[x]--;
+        siz[y]++;
+        parent[u] = y;
+    }
+    int Size(int u) {
+        return siz[Find(u)];
+    }
+} dsu(mxN);
+
+struct MST {
+    vector <pair<int, pair<int , int>>> edges;
+    vector <pair<int, pair<int , int>>> mstedges;
+    int nodes , mstcost;
+    void ProcessKruskal() {
+        sort(edges.begin() , edges.end());
+        int cnt = 0 ;
+        for (auto it : edges) {
+            int u = it.second.first;
+            int v = it.second.second;
+            int w = it.first;
+            if (!dsu.SameSet(u , v)) {
+                dsu.Union(u , v);
+                mstedges.push_back(make_pair(w , make_pair(u , v)));
+                cnt++ , mstcost += w;
+            }
+            if (cnt + 1 == nodes)
+                break ;
+        }
+    }
+    void Init(int n , const vector<pair<int , pair<int , int>>> &_edges) {
+        edges = _edges;
+        nodes = n;
+        mstcost = 0;
+        dsu.Makeset(nodes);
+    }
+    void Init(int n) {
+        nodes = n;
+        mstcost = 0;
+        dsu.Makeset(nodes);
+        edges.clear();
+    }
+    void AddEdge(int u, int v, int w) {
+        edges.push_back(make_pair(w , make_pair(u , v)));
+    }
+    void ShowEdgeList() {
+        for (auto it : edges) {
+            cout << it.second.first << " -> " << it.second.first << " " ;
+            cout << it.first << "\n";
+        }
+    }
+    void ShowMSTEdgeList() {
+        for (auto it : mstedges) { 
+            cout << it.second.first << " -> " << it.second.second << " ";
+            cout << it.first << "\n";
+        }    
+    }
+};
