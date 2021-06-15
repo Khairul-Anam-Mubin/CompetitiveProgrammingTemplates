24eff7a154c8aeecf321a2f647e3ddf69a96b5af
Khairul Anam Mubin <anam.mubin1999@gmail.com>
2021-06-15 10:23:39 +0600

    Update Strongly Connected Component.cpp

--- Graph Theory/Strongly Connected Component.cpp
+++ Graph Theory/Strongly Connected Component.cpp
@@ -1,3 +1,75 @@
+struct StronglyConnectedComponent {
+    vector <vector <int>> g , gr;
+    vector <bool> vis;
+    vector <int> order, sccid;
+    int nodes , edges , scc = 0;
+ 
+    void Init(int _nodes) {
+        nodes = _nodes;
+        edges = 0;
+        scc = 0;
+        g.clear();
+        gr.clear();
+        g.assign(nodes + 1 , {});
+        gr.assign(nodes + 1, {});
+        order.clear();
+        vis.assign(nodes + 1, 0);
+        sccid.resize(nodes + 1);
+    } 
+    void AddEdge(int u , int v) {
+        if (u == -1 || v == -1) return;
+        g[u].push_back(v);
+        gr[v].push_back(u);
+        edges++;
+    }
+    void Init() {
+        for (int i = 0; i <= nodes; i++) vis[i] = 0;
+    }
+    void Dfs1(int u) {
+        if (vis[u]) return;
+        vis[u] = 1;
+        for (int v : g[u]) Dfs1(v);
+        order.push_back(u);
+    }
+    void TopSort() {
+        Init();
+        for (int i = 0; i < nodes; i++) { 
+            if (!vis[i]) Dfs1(i);
+        }
+        reverse(order.begin() , order.end());
+    }
+    void Dfs2(int u, int id) {
+        if (vis[u]) return;
+        vis[u] = 1;
+        sccid[u] = id;
+        for (int v : gr[u]) Dfs2(v , id);
+    }
+    void KosarajuSCC() {
+        TopSort();
+        Init();
+        for (int i : order) {
+            if (!vis[i]) {
+                Dfs2(i , scc);
+                scc++;
+            }
+        }
+    }
+    int GetAns() {
+        vector <int> here(nodes + 1);
+        for (int i = 0; i < scc; i++ ) {
+            here[i] = 1;
+        }
+        for (int i = 0; i < nodes; i++) {
+            for (int j : g[i]) {
+                if (sccid[i] != sccid[j])
+                    here[sccid[j]] = 0;
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < scc; i++) ans += here[i];
+        return ans;
+    }
+};
 /*********************************Strongly Connected Component(Kosaraju_dfs)**************************************/
 
 /*...........................Finding strongly connected component............................*/
