b3b63d855853b69c5fe2ae35c6951fc66f543a50
Khairul Anam Mubin <anam.mubin1999@gmail.com>
2021-07-07 22:08:21 +0600

    Update Heavy Light Decomposition.cpp

--- Data Structure/Heavy Light Decomposition.cpp
+++ Data Structure/Heavy Light Decomposition.cpp
@@ -1,4 +1,100 @@
-// 1348 - Aladdin and the Return Journey
+
+/*
+    1. constructor
+    2. init
+    3. build
+    4. take careod INF9 and INF18
+*/
+template <typename T> struct SegmentTree {
+    vector <T> seg;
+    vector <T> lazy;
+    vector <T> ar;
+    int type , up;
+    SegmentTree() {
+        type = 0;
+        up = 0;
+    }
+    SegmentTree(int tp , int u) {
+        type = tp;
+        up = u;
+    }
+    void Init(int N) {
+        seg.assign(N << 2, 0);
+        lazy.assign(N << 2 , 0);
+    }
+    void Init(vector <T> &s) {
+        Init(s.size() + 1);
+        ar = s;
+    }
+    void PushDown(int cur , int left , int right) {
+        if (type == 0) {
+            if (up == 1) seg[cur] += (right - left + 1) * lazy[cur];
+            else seg[cur] = (right - left + 1) * lazy[cur];
+        } else {
+            if (up == 1) seg[cur] += lazy[cur];
+            else seg[cur] = lazy[cur];
+        }
+        if (left != right) {
+            if (up == 1) {        
+                lazy[cur << 1] += lazy[cur];  
+                lazy[cur << 1 | 1] += lazy[cur];
+            } else {
+                lazy[cur << 1] = lazy[cur];  
+                lazy[cur << 1 | 1] = lazy[cur];
+            }
+        }
+        lazy[cur] = 0;
+    }
+    T Merge(T x , T y) {
+        if (type == 0) return x + y;
+        if (type == 1) return max(x , y);
+        if (type == 2) return min(x , y);
+    }
+    void Build(int cur , int left , int right) {
+        lazy[cur] = 0;
+        if (left == right) {
+            seg[cur] = ar[left];
+            return;
+        }
+        int mid = (left + right) >> 1;
+        Build(cur << 1 , left , mid);
+        Build(cur << 1 | 1 , mid + 1 , right);
+        seg[cur] = Merge(seg[cur << 1] , seg[cur << 1 | 1]);
+    }
+    void Update(int cur , int left , int right , int pos , T val) {
+        Update(cur , left , right , pos , pos , val);
+    }
+    void Update(int cur , int left , int right , int l , int r , T val) {
+        if (lazy[cur] != 0) PushDown(cur , left , right);         
+        if (l > right || r < left) return;
+        if (left >= l && right <= r) {
+            if (up == 0) lazy[cur] = val;
+            else lazy[cur] += val;
+            PushDown(cur , left , right);
+            return ;
+        }
+        int mid = (left + right) >> 1;
+        Update(cur << 1 , left , mid , l , r , val);
+        Update(cur << 1 | 1 , mid + 1 , right , l , r , val);
+        seg[cur] = Merge(seg[cur << 1] , seg[cur << 1 | 1]);
+    }
+    T Query(int cur , int left , int right , int l , int r) {
+        if (l > right || r < left) {
+            if (type == 0) return 0;
+            if (type == 1) return -INF18;
+            if (type == 2) return INF18;
+        }
+        if (lazy[cur] != 0) PushDown(cur , left , right);
+        if (left >= l && right <= r) return seg[cur];
+        int mid = (left + right) >> 1;
+        T p1 = Query(cur << 1 , left , mid , l , r);
+        T p2 = Query(cur << 1 | 1 , mid + 1 , right , l , r);
+        return Merge(p1 , p2);
+    }
+};
+//for sum = 0, max = 1, min = 2, for assignment update send 0 or 1 for increment.
+SegmentTree <long long> T(0 , 0);
+
 
 /**************************HeavyLightDecomposition***********************/
 /*  1.  All nodes are number from 0 to n - 1  */ 
@@ -31,14 +127,17 @@ struct HeavyLightDecompose {
         chain_ind.resize(N);
         node_serial.resize(N);
         serial_node.resize(N);
+        return;
     }
     void Init(const vector <vector<int>> &_g) {
         Init(_g.size());
         g = _g;
+        return;
     }
     void AddEdge(int u , int v) {
         g[u].push_back(v) ;
         g[v].push_back(u) ;
+        return;
     }
     void TakeNodeVal(const vector <long long> &_node_val) {
         node_val = _node_val;
@@ -61,8 +160,10 @@ struct HeavyLightDecompose {
             parent[v] = u;
             depth[v] = depth[u] + 1;
             Dfs(v , u);
-            sub[u] += sub[v];   
+            sub[u] += sub[v];
+            
         }
+        return;
     }
     void HLD(int u , int par = -1) {
         if (chain_head[chain_no] == -1) chain_head[chain_no] = u;
@@ -86,9 +187,9 @@ struct HeavyLightDecompose {
                 HLD(v , u);
             }
         }
+        return;
     }
     void Update(int p , int val) {
-        T.Update(1 , 0 , N - 1, node_serial[p] , -node_val[p]);
         T.Update(1 , 0 , N - 1, node_serial[p] , val);
         node_val[p] = val;
     }
@@ -105,3 +206,35 @@ struct HeavyLightDecompose {
         return ans;
     }
 } hd;
+int main() {
+    FasterIO
+    Test {
+        int n; cin >> n;
+        hd.Init(n);
+        vector <long long> pos;
+        for (int i = 0; i < n; i++) {
+            ll x; cin >> x;
+            pos.PB(x);
+        }
+        hd.TakeNodeVal(pos);
+        for (int i = 0; i < n - 1; i++) {
+            int u , v; cin >> u >> v;
+            hd.AddEdge(u , v);
+        }
+        hd.Build();
+        int q; cin >> q;
+        cout << "Case " << tc << ":\n";
+        while (q--) {
+            int x; cin >> x;
+            if (x == 0) {
+                int u , v; cin >> u >> v;
+                cout << hd.Query(u , v) << "\n";
+            } else if (x == 1) {
+                int node , val;
+                cin >> node >> val;
+                hd.Update(node , val);
+            }
+        }
+    }
+    return 0;
+}
