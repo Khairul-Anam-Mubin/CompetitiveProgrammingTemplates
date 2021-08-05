56b5e34f0a7c76343138b97d840a0c31bf1e1f57
Khairul Anam Mubin <anam.mubin@gmail.com>
2021-08-05 12:31:16 +0600

    Factorial Factorization modified

--- Number Theory/Factorial Factorization.cpp
+++ Number Theory/Factorial Factorization.cpp
@@ -1,61 +1,48 @@
-/**********************Factorial Factorization************************/
-/*.Given a number n , find all its factorial factorisations.*/
-
 #include <bits/stdc++.h>
 using namespace std ;
 
-#define mxN 100
+#define mxN 1000000
 
-bool isp[mxN + 10] ;
-vector <int> prime ;
-vector <pair <int , int>> fac[mxN + 10] ;
+bool isp[mxN + 10];
+vector <int> prime; // all the primes <= mxN
+vector <pair <int , int>> ff; // storing factorial factorization as p^q pair(p , q) 
 
-void sieve(){
-    isp[1] = 1 ;
-    for(int i = 4 ; i <= mxN ; i += 2)
-        isp[i] = true;
-    for(int i = 3 ; i * i <= mxN ; i += 2)
-        if(!isp[i])
-            for(int j = i * i ; j <= mxN ; j +=(i + i))
-                isp[j] = true;
-    prime.push_back(2);
-    for(int i = 3 ; i <= mxN ; i += 2)
-        if(!isp[i])
-            prime.push_back(i);
-}
-void facFactorize(int n , vector <pair<int , int>> &v) {
-    for(int i = 0 ; i < prime.size() && prime[i] <= n ; i++) {
-        int x = n ;
-        int freq = 0 ;
-        while(x / prime[i]) {
-            freq += x / prime[i] ;
-            x /= prime[i] ;
-        }
-        v.push_back(make_pair(prime[i] , freq)) ;
-    }
-}
-void precal() {
-    for(int i = 2 ; i <= mxN ; i++) {
-        facFactorize(i , fac[i]) ;
-    }
-}
-int main() {
-    sieve() ;
-    precal() ;
-    int tc , test = 0 ;
-    cin >> tc ;
-    while(tc--) {
-        int n ;
-        cin >> n ;
-        cout << "Case " << ++test << ": " ;
-        cout << n << " = " ;
-        for(int i = 0 ; i < fac[n].size() ; i++) {
-            if(i + 1 == fac[n].size()) {
-                cout << fac[n][i].first << " (" << fac[n][i].second << ")\n" ;
-            } else {
-                cout << fac[n][i].first << " (" << fac[n][i].second << ") * " ;
+void Sieve() { 
+    isp[0] = isp[1] = 1 ;
+    for (int i = 4 ; i <= mxN ; i += 2)
+        isp[i] = 1;
+    for (int i = 3 ; i * i <= mxN ; i += 2) {
+        if (isp[i] == 0) {
+            for (int j = i * i ; j <= mxN ; j +=(i + i)) {
+                isp[j] = 1;
             }
         }
     }
-    return 0 ;
+    prime.push_back(2);
+    for (int i = 3 ; i <= mxN ; i += 2) {
+        if (isp[i] == 0) {
+            prime.push_back(i);
+        }
+    }
+}
+void FactFactorize(int n) {
+    for (int i = 0 ; i < prime.size() && prime[i] <= n ; i++) {
+        int x = n;
+        int freq = 0;
+        int tmp = prime[i];
+        while (x / tmp) {
+            freq += x / tmp ;
+            tmp *= prime[i] ;
+        }
+        ff.push_back(make_pair(prime[i] , freq)) ;
+    }
+}
+int main() {
+    Sieve();
+    int n ; cin >> n;
+    FactFactorize(n);
+    for (int i = 0; i < ff.size(); i++) {
+        cout << ff[i].first << "^" << ff[i].second << "\n";
+    }
+    return 0;
 }
