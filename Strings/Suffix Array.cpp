211ebf7688bcb00aad2e21cb59fc708dfc8a59bf
Khairul Anam Mubin <anam.mubin1999@gmail.com>
2022-08-27 22:54:18 +0600

    Update Suffix Array.cpp

--- Strings/Suffix Array.cpp
+++ Strings/Suffix Array.cpp
@@ -1,19 +1,17 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 template<typename T, bool maximum_mode = false>
-struct RMQ {
+struct RangeMinQuery {
     int n = 0;
     vector<vector<T>> sptab;
-    static int Largest_Bit(int x) {
+    static int LargestBit(int x) {
         return 31 - __builtin_clz(x);
     }
     static T Better(T a, T b) {
         return maximum_mode? max(a , b): min(a, b);
     }
+    // O(NlogN)
     void Build(const vector<T> &values) {
         n = int(values.size());
-        int levels = Largest_Bit(n) + 1;
+        int levels = LargestBit(n) + 1;
         sptab.resize(levels);
         for (int k = 0; k < levels; k++) sptab[k].resize(n - (1 << k) + 1);
         if (n > 0) sptab[0] = values;
@@ -23,14 +21,15 @@ struct RMQ {
             }
         }
     }
-    int query_value(int a, int b) const {  // Complexity : O(1)
+    // O(1)
+    int Query(int a, int b) const {  
         assert(0 <= a && a < b && b <= n);
-        int level = Largest_Bit(b - a);
+        int level = LargestBit(b - a);
         return Better(sptab[level][a], sptab[level][b - (1 << level)]);
     }
 };
-RMQ<int>rmq;
-// Complexity O(N * LogN) [radix_sort used]
+RangeMinQuery<int>rmq;
+// Complexity O(N * LogN)
 struct SuffixArray{
     string s;
     int n;
@@ -50,54 +49,49 @@ struct SuffixArray{
         BuildLCPArray();
         rmq.Build(lcp);
     }
+    // O(NLogN)
     void BuildSuffixArray() {
-        vector <pair<char,int>> a(n);
-        for (int i = 0; i < n; i++) a[i] = {s[i] , i};
-        sort(a.begin() , a.end());
-        for (int i = 0; i < n; i++) sar[i] = a[i].second;
+        vector <int> cnt(256, 0), pos(256);
+        for (int i = 0; i < n; i++) cnt[s[i]]++;
+        pos[0] = 0;
+        for (int i = 1; i < 256; i++) pos[i] = pos[i - 1] + cnt[i - 1];
+        for (int i = 0; i < n; i++) {
+            sar[pos[s[i]]] = i;
+            pos[s[i]]++;
+        }
         rank[sar[0]] = 0;
         for (int i = 1; i < n; i++) {
             rank[sar[i]] = rank[sar[i - 1]];
-            if (a[i].first != a[i - 1].first) rank[sar[i]]++;
+            if (s[sar[i]] != s[sar[i - 1]]) rank[sar[i]]++;
         }
+        pos.resize(n);
         for (int k = 0; (1 << k) < n; k++) {
-            vector <pair<pair<int , int>, int>> a(n);
-            for (int i = 0; i < n; i++) a[i] = {{rank[i] , rank[(i + (1 << k)) % n]} , i};
-            Radix_Sort(a);
-            for (int i = 0; i < n; i++) sar[i] = a[i].second;
-            rank[sar[0]] = 0;
-            for (int i = 1; i < n; i++) {
-                rank[sar[i]] = rank[sar[i - 1]];
-                if (a[i].first != a[i - 1].first) rank[sar[i]]++;
+            for (int i = 0; i < n; i++) sar[i] = (sar[i] - (1 << k) + n) % n;
+            cnt.assign(n, 0);
+            for (int x : rank) cnt[x]++;
+            pos[0] = 0;
+            for (int i = 1; i < n; i++) pos[i] = pos[i - 1] + cnt[i - 1];
+            vector <int> sar_new(n);
+            for (int x : sar) {
+                int i = rank[x];
+                sar_new[pos[i]] = x;
+                pos[i]++;
             }
+            sar = sar_new;
+            vector <int> rank_new(n);
+            rank_new[sar[0]] = 0;
+            for (int i = 1; i < n; i++) {
+                rank_new[sar[i]] = rank_new[sar[i - 1]];
+                pair <int, int> prev = {rank[sar[i - 1]], rank[(sar[i - 1] + (1 << k)) % n]};
+                pair <int, int> cur = {rank[sar[i]] , rank[(sar[i] + (1 << k)) % n]};
+                if (prev != cur) rank_new[sar[i]]++;
+            }
+            rank = rank_new;
         }
-    }
-    void Radix_Sort(vector <pair<pair<int , int>, int>> &a) {
-        vector <int> cnt(n, 0) , pos(n);
-        vector <pair<pair<int , int> , int>> a_new(n);
-        for (auto it: a) cnt[it.first.second]++;
-        pos[0] = 0;
-        for (int i = 1; i < n; i++) pos[i] = pos[i - 1] + cnt[i - 1];
-        for (auto it : a) {
-            int i = it.first.second;
-            a_new[pos[i]] = it;
-            pos[i]++;
-        }
-        a.swap(a_new);
-        cnt.assign(n , 0);
-        for (auto it: a) cnt[it.first.first]++;
-        pos[0] = 0;
-        for (int i = 1; i < n; i++) pos[i] = pos[i - 1] + cnt[i - 1];
-        for (auto it : a) {
-            int i = it.first.first;
-            a_new[pos[i]] = it;
-            pos[i]++;
-        }
-        a.swap(a_new);
     }
     // algorithm of Kasai, Arimura, Arikawa, Lee and Park.
     // Complexity O(n)
-    // Longest Common prefix
+    // Longest Common prefix of adjacent suffixes
     void BuildLCPArray() {
         int k = 0;
         for (int i = 0; i < n - 1; i++) {
@@ -108,49 +102,48 @@ struct SuffixArray{
             k = max(k - 1, 0);
         }
     }
-    int get_lcp_from_ranks(int a, int b) const {
+    // O(1)
+    int GetLcpFromRanks(int a, int b) const {
         if (a == b) return n - sar[a];
         if (a > b) swap(a, b);
-        return rmq.query_value(a + 1, b + 1);
+        return rmq.Query(a + 1, b + 1);
     }
-    int get_lcp(int a, int b) const {
+    // O(1)
+    int GetLcp(int a, int b) const {
         if (a >= n || b >= n) return 0;
         if (a == b) return n - a;
-        return get_lcp_from_ranks(rank[a], rank[b]);
+        return GetLcpFromRanks(rank[a], rank[b]);
     }
-    // Compares the substrings starting at `a` and `b` up to `length` in O(1).
-    int compare(int a, int b, int length = -1) const {
+    // Compares the substrings starting at `a` and `b` up to `length`
+    // O(1)
+    int Compare(int a, int b, int length = -1) const {
         if (length < 0) length = n;
         if (a == b) return 0;
-        int common = get_lcp(a, b);
+        int common = GetLcp(a, b);
         if (common >= length) return 0;
         if (a + common >= n || b + common >= n) {
             return a + common >= n ? -1 : 1;
         }
         return s[a + common] < s[b + common] ? -1 : (s[a + common] == s[b + common] ? 0 : 1);
     }
-    void ShowSuffixArray() {
-        cout << "Rank LCP    SA    Suffixes\n";
-        for (int i = 0; i < n; i++) {
-            cout << rank[i] << "    " << lcp[i] << "     " << sar[i] << "     " << s.substr(sar[i] , n - sar[i]) << "\n";
-        }
-    }
-    // Extends Templates(Queries)...
-    // Binary search Complexity O(logN)
+    // O(N)
     long long NumOfUniqueSubStr() {
         long long sum = 0;
-        for (int i = 0; i < lcp.size(); i++) sum += lcp[i];
-        
-        long long sz = ((n - 1) * n) / 2;
-        return sz - sum;
+        for (int i = 1; i < lcp.size(); i++) sum += lcp[i];
+        long long sz = n - 1; // actual size of string
+        long long totalSubStrings = (sz * (sz + 1)) / 2LL;
+        return totalSubStrings - sum;
     }
+    // O(|t|*LogN)
     bool IsSubstr(string &t) {  
-        if ((int)t.size() > (int)s.size()) return false;
-        int low = Lower_Bound(t);
-        if (s.substr(sar[low] , min(int(t.size()) , n)) == t) return true;
+        int tlen = t.size();
+        if (tlen > n) return false;
+        int low = LowerBound(t);
+        if (low < n && n - sar[low] >= tlen && s.compare(sar[low] , tlen, t) == 0) return true;
         return false;
     }
-    int Lower_Bound(string &t) {
+    // O(|t|*LogN)
+    int LowerBound(string &t) {
         int low = 0 ,high = n ,tlen = t.size();
         while (low < high) {
             int mid = (low + high) >> 1;
@@ -161,7 +154,8 @@ struct SuffixArray{
         }
         return low;
     }
-    int Upper_Bound(string &t) {
+    // O(|t|*LogN)
+    int UpperBound(string &t) {
         int low = 0 ,high = n ,tlen = t.size();
         while (low < high) {
             int mid = (low + high) >> 1;
@@ -172,13 +166,16 @@ struct SuffixArray{
         }
         return low;
     }
+    // O(|t|*LogN)
     int SubstrOccurences(string &t) {
-        int low = Lower_Bound(t) , up = Upper_Bound(t);
-        if (low == up || s.substr(sar[low] , min(int(t.size()) , n)) != t) return 0;
+        int tlen = t.size();
+        int low = LowerBound(t) , up = UpperBound(t);
+        if (low == up || s.compare(sar[low] , tlen , t) != 0) return 0;
         return (up - low);
     }
     // Longest common substring of two string
-    string LCS(string &a, string &b) {
+    // O(NLogN)
+    string LongestCommonSubstring(string &a, string &b) {
         int alen = a.size();
         Init(a + "&" + b);
         int tot = sar.size();
@@ -196,5 +193,11 @@ struct SuffixArray{
         }
         if (mxid != -1) return s.substr(mxid , mx);
         return "";
+    }
+    void ShowSuffixArray() {
+        cout << "Rank LCP    SA    Suffixes\n";
+        for (int i = 0; i < n; i++) {
+            cout << rank[i] << "    " << lcp[i] << "     " << sar[i] << "     " << s.substr(sar[i] , n - sar[i]) << "\n";
+        }
     }
 };
