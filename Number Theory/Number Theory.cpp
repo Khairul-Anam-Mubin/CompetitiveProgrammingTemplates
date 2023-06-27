c146b205c54f5a1f19233a0321649893b401fead
Khairul Anam Mubin <anam.mubin1999@gmail.com>
2023-06-27 14:51:37 +0600

    Update Number Theory.cpp

--- Number Theory/Number Theory.cpp
+++ Number Theory/Number Theory.cpp
@@ -477,3 +477,33 @@ pair<int, int> fib (int n) {
     else
         return {c, d};
 }
+// finding primitive root of a prime number p
+int powmod (int a, int b, int p) {
+    int res = 1;
+    while (b)
+        if (b & 1)
+            res = int (res * 1ll * a % p),  --b;
+        else
+            a = int (a * 1ll * a % p),  b >>= 1;
+    return res;
+}
+
+int generator (int p) {
+    vector<int> fact;
+    int phi = p-1,  n = phi;
+    for (int i=2; i*i<=n; ++i)
+        if (n % i == 0) {
+            fact.push_back (i);
+            while (n % i == 0)
+                n /= i;
+        }
+    if (n > 1)
+        fact.push_back (n);
+
+    for (int res=2; res<=p; ++res) {
+        bool ok = true;
+        for (size_t i=0; i<fact.size() && ok; ++i)
+            ok &= powmod (res, phi / fact[i], p) != 1;
+        if (ok)  return res;
+    }
+    return -
